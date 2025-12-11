451c6f3f893721723fe37f9832612cef2269e28c
vikasadari69-source <vikasadari69@gmail.com>
2025-12-11 12:44:10 +0530

    fix: replace path alias with relative path to fix module resolution

--- server/storage.ts
+++ server/storage.ts
@@ -1,4 +1,4 @@
-import { type User, type InsertUser } from "@shared/schema";
+import { type User, type InsertUser } from "../shared/schema";
 import { randomUUID } from "crypto";
 
 // modify the interface with any CRUD methods
